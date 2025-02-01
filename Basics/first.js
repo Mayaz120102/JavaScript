e6decd92e343b78f13a3554923d54ce9a5f708f9
Mayaz120102 <abrarmayaz2002@gmail.com>
2025-02-01 19:19:15 +0600

    basics of js

---
+++ Basics/first.js
@@ -0,0 +1,22 @@
+// const arrowmul = (a,b)=>{
+//     console.log(a*b); //a*b;
+//  }
+
+// function myfunction(){
+//     console.log("we are flagmanioas");
+// }
+
+// myfunction();
+// function sum(a,b){
+//     s= a+b
+//     return s;
+//     console.log(s);
+// }
+// console.log(sum(1,2))
+
+let nums = [2,3,4,5,6];
+
+nums.forEach((num) => {
+    console.log(num*num);
+
+});
